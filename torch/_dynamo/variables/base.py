3586496de9d0863b37185d86e4ca26d397451b58
zhxchen17 <zhxchen17@fb.com>
2026-05-06 17:38:59 -0700

    [dynamo] Python codegen for simple fullgraph case POC. [1/n]
    Summary:
    
    This PR adds a feature to optionally generate python wrapper code in addition to the bytecode wrapper, such that it has the same behavior for simple
    fullgraph cases. This is useful when we are compiling a module/function and want all part to be savable as Python for debugability or portability
    reasons.
    
    This option is introduced by opting in with `torch._dynamo.generate_pycode=True` and will be enabled when fullgraph_capture() is used. NOTE that
    to minimize the landing risk, user also need to set use_python_codegen=True to activate this at runtime.
    
    Sample generated python wrapper code:
    
    ```
    __arg0 = self._modules['linear']._parameters['weight']
    __arg1 = self._modules['linear']._parameters['bias']
    __arg2 = x
    __graph_out = __compiled_fn_<ID>(__arg0, __arg1, __arg2)
    __stack0 = __graph_out[0]
    __ret = __stack0""",
    ```
    
    Test Plan:
    
    pytest test/dynamo/test_pycode.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 29b61ac1a05b5dcda84aa9aef85d65defe83f0d1
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/182090

--- torch/_dynamo/variables/base.py
+++ torch/_dynamo/variables/base.py
@@ -599,6 +599,9 @@ class VariableTracker(metaclass=VariableTrackerMeta):
     def reconstruct(self, codegen: PyCodegen) -> None:
         raise NotImplementedError
 
+    def reconstruct_pycode(self, codegen: PyCodegen) -> str:
+        raise NotImplementedError
+
     def unpack_var_sequence(self, tx: Any) -> list[VariableTracker]:
         raise NotImplementedError
 
--- torch/_dynamo/variables/user_defined.py
+++ torch/_dynamo/variables/user_defined.py
@@ -1474,6 +1474,13 @@ class UserDefinedObjectVariable(UserDefinedVariable):
             return side_effects.is_modified(self._base_vt)
         return False
 
+    def reconstruct_pycode(self, codegen):
+        if self.source:
+            return self.source.reconstruct_pycode(codegen)
+        raise NotImplementedError(
+            "Python codegen not implemented yet for sourceless UserDefinedObjectVariable"
+        )
+
     def python_type(self) -> type:
         return self.value_type  # type: ignore[return-value]
 
